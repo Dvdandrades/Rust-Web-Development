eccac429288c2c68268d2a4f7d94b4a6b6f4f32a
Dvdandrades <dvdandrades@gmail.com>
2026-04-20 09:52:30 +0200

    update actix project

--- rust-actix-web/src/main.rs
+++ rust-actix-web/src/main.rs
@@ -1,24 +1,25 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use serde::Serialize;
+mod handlers;
+mod models;
+mod routes;
 
-#[derive(Serialize)]
-struct Message {
-    message: String,
-}
+use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+use std::env;
 
-async fn greet() -> impl Responder {
-    HttpResponse::Ok().json(Message {
-        message: "Hello from Rust API!".to_string(),
-    })
-}
+use routes::user_routes::init;
 
-// Main function to run the HTTP server
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+
+    println!("Server running at http://{}:{}", host, port);
+
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(greet)) // Route "/" to the greet function
+        App::new().configure(init)
     })
-    .bind("127.0.0.1:8080")? // Bind the server to localhost:8080
+    .bind(format!("{}:{}", host, port))?
     .run()
     .await
 }
